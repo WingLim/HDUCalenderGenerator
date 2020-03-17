3879712b4b0a5990fca5eb3327cd5868c548d83b
WingLim <643089849@qq.com>
2020-03-17 08:41:39 +0800

    fix: host format

--- server.py
+++ server.py
@@ -17,4 +17,4 @@ def hello():
         return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host=0.0.0.0, port=9898)
+    app.run(debug=True, host='0.0.0.0', port=9898)
