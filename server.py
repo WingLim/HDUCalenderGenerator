f731881b7636bd33a607f559f83e86e1aa09e606
WingLim <643089849@qq.com>
2020-03-17 08:35:24 +0800

    feat: use flask as server

--- server.py
+++ server.py
@@ -1,31 +1,20 @@
-import asyncio
+from flask import Flask, render_template, request, make_response
 import hdu_ics
+app = Flask(__name__)
 
-from aiohttp import web
+@app.route('/', methods=['POST', 'GET'])
+def hello():
+    if request.method == 'POST':
+        account = request.form['account']
+        password = request.form['password']
+        spider = hdu_ics.Schedule2ICS(account, password, 1)
+        result = spider.run()
+        response = make_response(result)
+        response.headers['Content-Type'] = 'text/calendar'
+        response.headers['Content-Disposition'] = "attachment; filename=\"{}.ics\"".format(account)
+        return response
+    else:
+        return render_template('index.html')
 
-routes = web.RouteTableDef()
-
-@routes.get('/ics')
-async def hello(request):
-    await asyncio.sleep(0.1)
-    html = """
-        <h1>Wlecome to use HDU Calender Generator</h1>
-    """
-    return web.Response(body='{}'.format(html), headers={'content-type': 'text/html'})
-
-@routes.get('/ics/{account}/{password}')
-async def entry(request):
-    await asyncio.sleep(0.1)
-    account = request.match_info['account']
-    password = request.match_info['password']
-    spider = hdu_ics.Schedule2ICS(account, password, 1)
-    result = spider.run()
-    return web.Response(body='{}'.format(result),
-                        headers={'content-type': 'text/calendar',
-                                 'content-disposition': "attachment; filename=\"{}.ics\"".format(account)
-                                 }
-                        )
-
-app = web.Application()
-app.add_routes(routes)
-web.run_app(app, port=9898)
+if __name__ == "__main__":
+    app.run(debug=True, port=9898)
