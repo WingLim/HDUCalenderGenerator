40103b8d5b11dab31776069c8e158b6f0af3bbae
WingLim <643089849@qq.com>
2020-03-17 11:29:24 +0800

    fix: transform str list to int list

--- info.example.py
+++ info.example.py
@@ -3,6 +3,7 @@ import os
 
 if 'SEMESTER_START' in os.environ:
     start = os.environ['SEMESTER_START'].split('-')
+    start = list(map(int, start))
     if len(start) == 3:
         semester_start = date(start[0], start[1], start[2])
 else:
