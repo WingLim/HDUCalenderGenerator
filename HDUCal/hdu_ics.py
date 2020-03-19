e8c8e5365154680ceccbf33fdb7654882dd3e428
WingLim <643089849@qq.com>
2020-03-19 16:57:20 +0800

    feat: parse json course timeinfo

--- HDUCal/hdu_ics.py
+++ HDUCal/hdu_ics.py
@@ -35,7 +35,7 @@ class Schedule2ICS:
             12: time(20,10)
         }
     
-    def parseWeek(self, timeinfo):
+    def parse_week(self, timeinfo):
         """解析课程每周信息
         Args:
             timeinfo: 课程的时间信息
@@ -67,10 +67,25 @@ class Schedule2ICS:
             data['start'] = int(dat[0])
             data['end'] = int(dat [1])
         return data
+    
+    def parse_time(self, timeinfo):
+        course_period_regrx = re.compile(r'(\d+)')
+        timeinfo = timeinfo.split('{')[0]
+        course_period_list = course_period_regrx.findall(timeinfo, 1)
+        # 一共几节课
+        course_period_num = len(course_period_list)
+        # 第一节课是第几节
+        course_period_start = int(course_period_list[0])
+        # 最后一节是第几节
+        course_period_end = int(course_period_list[course_period_num - 1])
+        # 开始上课时间
+        dtstart_time = self.dirt_week[course_period_start]
+        # 最后一节课上课时间
+        dtend_time = self.dirt_week[course_period_end]
+        return dtstart_time, dtend_time
 
-    def parseDay(self, timeinfo):
-        """解析课程日期
-        解析课程在星期几上课
+    def parse_day(self, timeinfo):
+        """解析课程在星期几上课
         Args:
             timeinfo: 课程的时间信息
         Returns:
@@ -88,7 +103,7 @@ class Schedule2ICS:
         }
         return weekday[word]
 
-    def parseSemesterStart(self, semester_start):
+    def parse_semester_start(self, semester_start):
         if isinstance(semester_start, str):
             start = semester_start.split('-')
             start = list(map(int, start))
@@ -97,7 +112,7 @@ class Schedule2ICS:
             result = semester_start
         return result
 
-    def exportCourse(self, response):
+    def export_course(self, response):
         """导出课程信息
         Args:
             response: 请求后返回的响应
@@ -148,7 +163,23 @@ class Schedule2ICS:
         # print(raw_courses)
         return raw_courses
     
-    def cookCourse(self, export_courses, semester_start):
+    def get_key(self, value):
+        return [k for k,v in self.dirt_week.items() if v == value]
+
+    def cook_course_json(self, export_courses):
+        result = []
+        for one in export_courses:
+            info = {}
+            t = one['timeinfo']
+            info['week'] = self.parse_week(t)
+            info['weekday'] = self.parse_day(t)
+            info['start'], info['end'] = (i.strftime('%H:%M') for i in self.parse_time(t)) 
+            one['timeinfo'] = info
+            result.append(one)
+        return result
+             
+    
+    def cook_courses(self, export_courses, semester_start):
         """处理课程信息为 ical
         将导出后的课程信息转化成 ical 格式
         Args:
@@ -160,7 +191,8 @@ class Schedule2ICS:
         calt = icalendar.Calendar()
         calt['version'] = '2.0'
         for one in export_courses:
-            week = self.parseWeek(one['timeinfo'])
+            t = one['timeinfo']
+            week = self.parse_week(t)
             # 课程开始周
             week_start = week['start']
             # 课程结束周
@@ -169,27 +201,15 @@ class Schedule2ICS:
             interval = week['flag']
             # 本学期该课程上课总数
             count = (week_end - week_start + 1) if interval == 1 else (((week_end - week_start) / 2) + 1)
-            course_weekday = self.parseDay(one['timeinfo'])
-            
-            timeinfo = one['timeinfo'].split('{')[0]
-            course_period_regrx = re.compile(r'(\d+)')
-            course_period_list = course_period_regrx.findall(timeinfo, 1)
-            # 一共几节课
-            course_period_num = len(course_period_list)
-            # 第一节课是第几节
-            course_period_start = int(course_period_list[0])
-            # 最后一节是第几节
-            course_period_end = int(course_period_list[course_period_num - 1])
+            course_weekday = self.parse_day(t)
             
             # 每节课持续时间
             lesson_time = relativedelta(minutes=45)
             # 课程日期
             dt_date = semester_start + relativedelta(weeks=(week_start - 1)) + relativedelta(
-            days=(course_weekday - 1))
+                days=(course_weekday - 1))
             # 开始上课时间
-            dtstart_time = self.dirt_week[course_period_start]
-            # 最后一节课上课时间
-            dtend_time = self.dirt_week[course_period_end]
+            dtstart_time, dtend_time = self.parse_time(t)
             
             # 上课日期时间
             dtstart_datetime = datetime.combine(dt_date, dtstart_time, tzinfo=pytz.timezone("Asia/Shanghai"))
@@ -207,7 +227,7 @@ class Schedule2ICS:
             event.add('location', one['location'])
             # 详细信息
             event.add('description',
-                  '第{}-{}节\r\n教师： {}\r\n教室: {}'.format(course_period_start, course_period_end, one['teacher'],
+                  '第{}-{}节\r\n教师： {}\r\n教室: {}'.format(self.get_key(dtstart_time), self.get_key(dtend_time), one['teacher'],
                                                        one['location']))
             event.add('dtstart', dtstart_datetime)
             event.add('dtend', dtend_datetime)
@@ -225,10 +245,10 @@ class Schedule2ICS:
             # 跳转到个人课表页面，获取 HTML 内容
             self.cas.headers['Referer'] = self.url + 'xs_main.aspx?xh=' + self.account
             response = self.cas.s.get(self.url + self.cas.schedule_url, headers=self.cas.headers)
-            export_courses = self.exportCourse(response)
+            export_courses = self.export_course(response)
             if filetype == 'ics':
-                semester_start = self.parseSemesterStart(semester_start)
-                calt = self.cookCourse(export_courses, semester_start)
+                semester_start = self.parse_semester_start(semester_start)
+                calt = self.cook_courses(export_courses, semester_start)
                 if not self.isserver:
                     with open('output.ics', 'w+', encoding='utf-8', newline='') as file:
                         file.write(calt.to_ical().decode('utf-8'.replace('\r\n', '\n')).strip())
@@ -236,15 +256,16 @@ class Schedule2ICS:
                     return calt.to_ical().decode('utf-8'.replace('\r\n','\n').strip())
             # json 格式只用于 API 服务
             elif filetype == 'json' and self.isserver:
+                result = self.cook_course_json(export_courses)
                 if save:
                     # 如果保存，则写入到 static/学号.json 中
                     filename = self.account + '.json'
                     with open('data/' + filename, 'w+', encoding='utf-8', newline='') as file:
-                        file.write(str(export_courses).replace("'",'"'))
+                        file.write(str(result).replace("'",'"'))
                     return filename
                 else:
                     # 不保存则直接返回 list
-                    return export_courses
+                    return result
 
 if __name__ == "__main__":
     spider = Schedule2ICS(info.account, '777')
